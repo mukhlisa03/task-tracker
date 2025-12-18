26e1206f51865512ab23963f96d00d7c5f72114b
Mukhlisa <mukhlisamakhmaraimova03@gmail.com>
2025-12-18 19:04:14 +0900

    feat: task-tracker project started

--- src/app/app.config.ts
+++ src/app/app.config.ts
@@ -1,5 +1,8 @@
 import { ApplicationConfig, provideBrowserGlobalErrorListeners } from '@angular/core';
 import { provideClientHydration, withEventReplay } from '@angular/platform-browser';
+import { createRequire } from 'module';
+
+const require = createRequire(import.meta.url);
 
 export const appConfig: ApplicationConfig = {
   providers: [
---
+++ src/app/task-add/task-add.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-task-add',
+  imports: [],
+  templateUrl: './task-add.html',
+  styleUrl: './task-add.css',
+})
+export class TaskAdd {
+
+}
